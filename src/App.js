d944cea02dd59d6e5aba3b1a9a270a332e269d16
Louie Fitzpatrick <fitz4492@gmail.com>
2020-07-10 01:41:11 -0500

    basic react app set up

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Home } from './pages/home';
+import './styles/reset.scss';
+import './styles/styles.scss';
+
+export const App = () => (
+  <div className="App">
+    <Home />
+  </div>
+);
